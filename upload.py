d1dc4d621d3b21463d0d360e7e907ae96167d5ac
Mayuresh123 <56029669+mayuresh714@users.noreply.github.com>
2021-03-05 11:19:25 +0530

    Update upload.py

--- upload.py
+++ upload.py
@@ -1,12 +1,28 @@
 import requests
 import base64
-url="https://api.imgbb.com/1/upload"
-with open("butter.jpeg", "rb") as file:
-    payload = {
-        "key": "4a05d0ef6da23523ac0b6c93eb131bb7",
-        "image": base64.b64encode(file.read()),
-    }
-    res = requests.post(url, payload)
+url="https://api.imgbb.com/1/upload"  ### using imgbb
+
+def upload_and_get_link(filename):        ### put address here of image file
+    with open(filename, "rb") as file:
+        payload = {
+            "key": "4a05d0ef6da23523ac0b6c93eb131bb7",
+            "image": base64.b64encode(file.read()),
+        }
+        res = requests.post(url, payload)    ##3uploading the image
+        abc  = res.json()
+        return abc['data']['url']        #### get hhtp url of it..
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+"""    
 with open("curd.jpg", "rb") as file:
     payload = {
         "key": "4a05d0ef6da23523ac0b6c93eb131bb7",
@@ -24,4 +40,4 @@ with open("Paneer.jpg", "rb") as file:
         "key": "4a05d0ef6da23523ac0b6c93eb131bb7",
         "image": base64.b64encode(file.read()),
     }
-    res = requests.post(url, payload)
+    res = requests.post(url, payload)"""
